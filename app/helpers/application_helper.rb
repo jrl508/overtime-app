5cbd3182c63c66d25203a35dbf023260350657a9
jrl508 <jrl47@yahoo.com>
2018-09-11 19:29:46 -0400

    utilized application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+    def nav_items 
+        [
+            {
+              url: root_path,
+              title: 'Home'
+            },
+            {
+              url: posts_path,
+              title: 'Time Entries'
+            }
+    
+      ]
+      end
+
+    def nav_helper tag_type, style
+
+        nav_links = ''
+  
+        nav_items.each do |item|
+  
+          nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"  
+        end
+  
+        nav_links.html_safe
+    end
+
+    def active? path
+        "active" if current_page?(path)
+    end   
 end
