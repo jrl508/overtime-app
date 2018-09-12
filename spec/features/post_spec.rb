b08d977253054c0b52bd1dd07606c426bf3d61d9
jrl508 <jrl47@yahoo.com>
2018-09-12 19:01:00 -0400

    implemented core layout and refactored index action

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
+    before do
+        @user = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "J'onnz")
+        login_as(@user, :scope => :user)
+    end
     describe 'index' do
         it "can be reached" do
             visit posts_path
@@ -11,12 +15,17 @@ describe 'navigate' do
             visit posts_path
             expect(page).to have_content(/Posts/)  
         end
+
+        it 'has a list of posts' do
+            post1 = Post.create(date: Date.today, rationale: "Post1", )
+            post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+            visit posts_path
+            expect(page).to have_content(/Post1|Post2/)
+        end
     end
 
     describe "creation" do
-        before do
-            user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-            login_as(user, :scope => :user)        
+        before do      
             visit new_post_path
         end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale:"something")
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
+      @post = Post.create(date: Date.today, rationale:"something", user: user)
     end
     
-    it "can be created" do
+    it 'can be created' do
       expect(@post).to be_valid
     end
     
