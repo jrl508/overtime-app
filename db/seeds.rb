d42916ea11a86bf9870b284329a0b03f0aa993c7
jrl508 <jrl47@yahoo.com>
2019-01-08 23:43:22 -0500

    implemented kaminari pagination gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,17 @@ puts "1 Admin User created"
 
 puts "1 User created"
 
-20.times do |post|
+50.times do |post|
 	Post.create!(date: Date.today, 
 				rationale: "#{post} rationale content", 
 				user_id: @user.id, 
 				overtime_request: 2.5)
 end
 
-puts "20 Posts have been created"
+puts "50 Posts have been created"
 
-20.times do |audit_log|
+50.times do |audit_log|
 	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
   end
   
-  puts "20 audit logs have been created"
+puts "50 audit logs have been created"
