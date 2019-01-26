ece39f4ea83c9a633ffffc667a94b817044a4993
jrl508 <jrl47@yahoo.com>
2019-01-25 19:31:33 -0500

    added comfirmation buttons for employee homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,9 @@ end
 
 puts "50 Posts have been created"
 
-50.times do |audit_log|
-	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-  end
-  
-puts "50 audit logs have been created"
+
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 audit logs have been created"
