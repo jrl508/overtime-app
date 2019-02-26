b778d2387a638600ebcd0a2047663e2b4bf23d7c
jrl508 <jrl47@yahoo.com>
2019-02-26 15:32:42 -0500

    Implemented nil guard for callback

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -45,7 +45,7 @@ describe 'navigate' do
     it 'has a link from the homepage' do
       visit root_path
 
-      click_link("New Entry")
+      click_link("Request Overtime")
       expect(page.status_code).to eq(200)
     end
   end
